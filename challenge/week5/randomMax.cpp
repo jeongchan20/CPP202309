2fa34cf4e57b9c638ff41b52b0d042f9cc3508e4
jeongchan20 <jungchan0358@naver.com>
2023-10-04 14:47:04 +0900

    Week5. 2차원배열

--- challenge/week5/randomMax.cpp
+++ challenge/week5/randomMax.cpp
@@ -8,33 +8,37 @@ int main()
 	int numlist[numCell][numCell]; //2차원 상자 생성!
 
 
-	for (int i = 0; i < numCell; i++)  //입력을 받기위해서 행을 for문으로 돌림
+	for (int i = 0; i < numCell; i++) //입력을 받기위해서 행을 for문으로 돌림
 	{
 		for (int j = 0; j < numCell; j++)  //입력을 받기위해서 열을 for문으로 돌림
 		{
-			int elem = rand() % 1000; //elem에 0~999에 변수를 넣음
-			numlist[i][j] =elem; //2차원 배열에 각각 1칸씩 elem를 값을 가지게됨
+			int elem = rand() % 1000;  //elem에 0~999에 변수를 넣음
+			numlist[i][j] = elem; //2차원 배열에 각각 1칸씩 elem를 값을 가지게됨
 			cout << i << "," << j << " : " << elem << endl;
 		}
 	}
 
 	cout << endl;
-	int max;
-	int maxI;
-	int maxJ;
+	int max=0;
+	int maxI=0;
+	int maxJ=0;
 
 	for (int i = 0; i < numCell; i++) //출력을 위해 이중 for문을 만듬 행을 돌림
 	{
-		for (int j = 0; j < numCell; j++) //출력을 위해 이중 for문을 만듬 열을 돌림
+		int j = 0; //j를 0으로 직접설정!!
+	
+		for (int value:numlist[i]) //for문을 변위기반으로 함 i줄 행을 끌고 와서 일일히 열을 비교시킴
 		{
-			int value = numlist[i][j]; //vlaue값을[i][j]로 선언
-			if (value > max) //value가 max가 크다면 vlaue를 max값으로 교체!하고 i와j도 maxI maxJ로 로 대입됨
+			
+			if (value > max) //value가 max보다 클경우!
 			{
-				max = value;
-				maxI = i;
-				maxJ = j;
+			{
+				max=value; //max에다가 value값을 대입함
+				maxI = i; //maxI에 i를 대입시킴
+				maxJ = j; 
 
 			}
+			j++;
 		}
 	}
 
