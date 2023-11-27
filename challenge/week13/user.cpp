d9c2630f9f04f8c175c3fa87c3210ddda75b6a37
jeongchan20 <jungchan0358@naver.com>
2023-11-27 22:26:08 +0900

    class 상속완료

---
+++ challenge/week13/user.cpp
@@ -0,0 +1,28 @@
+#include"user.h"
+
+//헤더파일 원형에 있는 함수식
+void User::DecreaseHP(int dec_hp) {
+	hp = hp - dec_hp;
+}
+
+int User::GetHP() {
+	return hp;
+}
+
+void User::IncreaseHP(int inc_hp) {
+	hp = hp + inc_hp;
+}
+
+void User::doAttack() {
+	cout << "공격합니다" << endl;
+}
+
+void Magician::doAttack() {
+	cout << "마법 사용" << endl;
+}
+
+void Warrior::doAttack() {
+	cout << "베기 사용" << endl;
+}
+
+
